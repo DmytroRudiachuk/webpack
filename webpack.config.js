6bd4e8dd8977e18bcfa3f9a89c1e352e8f447709
Dmytro Rudiachuk <keri19372@gmail.com>
2021-08-10 01:46:44 +0300

    add dev and build

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
-  entry: "js/main.js",
+  entry: "./js/main.js",
   output: {
-    filename: `js/${filename("js")}`,
+    filename: `./js/${filename("js")}`,
     path: path.resolve(__dirname, "app"),
   }
 }
